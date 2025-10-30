f50d4aabf7f6f60432d9a7ea8be9486570668e71
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2025-10-30 17:31:01 +0800

    fix I type don't get src2 pass add.c,fib.c,sum.c

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -27,7 +27,8 @@
 
 enum {
   TYPE_I, TYPE_U, TYPE_S,
-  TYPE_J,
+  TYPE_J, TYPE_R,
+  TYPE_B,
   TYPE_N, // none
 };
 
@@ -36,6 +37,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immB() do { *imm = SEXT((BITS(i,31,31)<<12)|(BITS(i,7,7)<<11)|(BITS(i,30,25)<<5)|(BITS(i,11,8)<<1),13);}while(0)
 
 #define immJ() do{*imm=getimmJ(i);}while(0)
 
@@ -57,20 +59,25 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
-    case TYPE_I: src1R();          immI(); break;
+    case TYPE_I: src1R(); src2R(); immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
 	case TYPE_J: immJ(); break;
+	case TYPE_R: src1R(); src2R(); break; 
+	case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
-//if(type==TYPE_J){
-//    printf("rd %d imm %X\n",*rd,*imm);
-//}
+  
+      printf("  |decode:| rd %d r1 %d r2 %d imm %X(%d)\n",*rd,rs1,rs2,*imm,*imm);
 }
 
 static int decode_exec(Decode *s) {
   s->dnpc = s->snpc;
+  void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
+  char buf[512];
+  disassemble(buf,sizeof(buf),s->pc,(uint8_t*)&s->isa.inst,s->snpc-s->pc);
+  printf(" |exec:%08X| %-25s \t",s->isa.inst,buf);
 
 #define INSTPAT_INST(s) ((s)->isa.inst)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
@@ -82,17 +89,39 @@ static int decode_exec(Decode *s) {
 
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+  INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
+
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
+  INSTPAT("??????? ????? ????? 010 ????? 00000 11", lw     , I,
+		  printf("load word %d from %X to r %d\n", Mr(src1 + imm, 4),src1+imm,rd);
+		  R(rd) = Mr(src1 + imm, 4));
+
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
-  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
+  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S,
+		  printf("save word %X to %X+%d=%X\n",src2,src1,imm,src1+imm);
+		  Mw(src1 + imm, 4, src2));
+
+  INSTPAT("??????? ????? ????? 011 ????? 00100 11", sltiu  , I, R(rd) = (src1<imm)?1:0); 
 
 
-  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm); 
+  INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi   , I,
+//		  printf("addi %d+%d=%d to r%d\n",src1,imm,src1+imm,rd);
+		  R(rd) = src1 + imm); 
+  INSTPAT("0000000 ????? ????? 000 ????? 01100 11", add    , I, 
+		  printf("add %d + %d = %d to r%d\n",src1,src2,src1+src2,rd);
+		  R(rd) = src1 + src2); 
+  INSTPAT("0100000 ????? ????? 000 ????? 01100 11", sub    , I, R(rd) = src1 - src2); 
+
   INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J,
 		  R(rd) = s->pc+4; s->dnpc=s->pc+imm);
 // setting the least-significant bit of the result to zero (see JALR p47)
   INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, 
 		  R(rd) = s->pc+4; s->dnpc=(src1+imm)&(~1));
+
+  INSTPAT("??????? ????? ????? 000 ????? 11000 11", beq    , B,
+		if(src1==src2)s->dnpc=s->pc+imm); 
+  INSTPAT("??????? ????? ????? 001 ????? 11000 11", bneq   , B,
+		if(src1!=src2)s->dnpc=s->pc+imm); 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));

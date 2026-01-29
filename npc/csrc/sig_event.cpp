12af40e40fa2cdc9ed86e4544af9936853c73f29
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-01-29 14:12:11 +0800

    impl perf counter to count each type inst cnt,cpi,percentage

--- npc/csrc/sig_event.cpp
+++ npc/csrc/sig_event.cpp
@@ -1,16 +1,6 @@
 #include "sig_event.hpp"
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-
-void set_logger_pattern_with_simtime(std::shared_ptr<spdlog::logger> logger);
-HandShakeDetector::HandShakeDetector() {
-  logger = spdlog::stdout_color_mt("HandShakeDetector");
-}
-void HandShakeDetector::init() {
-	set_logger_pattern_with_simtime(logger);
-	logger->set_level(spdlog::level::info);
-}
-
 auto _FullPath(const std::string &pathWithoutValidOrReady,
                const std::string &suffix = "") {
   return cpu_vpi_path_prefix() + pathWithoutValidOrReady + suffix;
@@ -20,33 +10,106 @@ auto _DebugPath(const std::string &pathWithoutValidOrReady,
   return "`cpu." + pathWithoutValidOrReady + suffix;
 }
 
-void HandShakeDetector::add(std::string barePath, std::string description) {
+void set_logger_pattern_with_simtime(std::shared_ptr<spdlog::logger> logger);
+HandShakeDetector::HandShakeDetector() {
+  logger = spdlog::stdout_color_mt("HandShakeDetector");
+}
+void HandShakeDetector::init() {
+  set_logger_pattern_with_simtime(logger);
+  logger->set_level(spdlog::level::info);
+}
+
+SignalHandle::SignalHandle(std::string barePath) {
+  handle = vpi_handle_by_name(
+      const_cast<PLI_BYTE8 *>(_FullPath(barePath).c_str()), nullptr);
+  if (!handle) {
+    spdlog::error("cannot find signal at path {}", _DebugPath(barePath));
+  }
+}
+
+void HandShakeDetector::add(std::string barePath, std::string description,
+                            callback_t onShake) {
   auto pathValid = _FullPath(barePath, "valid");
   auto pathReady = _FullPath(barePath, "ready");
   spdlog::debug("adding valid/ready pair: {}/{}", pathValid, pathReady);
-  vpiHandle hValid =
-      vpi_handle_by_name(const_cast<PLI_BYTE8 *>(pathValid.c_str()), nullptr);
-  if (!hValid) {
-    spdlog::error("cannot find valid signal at path {}",
-                  _DebugPath(barePath, "valid"));
-  }
-  vpiHandle hReady =
-      vpi_handle_by_name(const_cast<PLI_BYTE8 *>(pathReady.c_str()), nullptr);
-  if (!hReady) {
-    spdlog::error("cannot find ready signal at path {}",
-                  _DebugPath(barePath, "ready"));
-  }
+  auto hValid = SignalHandle(barePath + "valid");
+  auto hReady = SignalHandle(barePath + "ready");
 
-  spdlog::info("added watch for channel {} ({})", _DebugPath(barePath), description);
-  bus_list.emplace_back(hValid, hReady, description);
+  spdlog::info("added watch for channel {} ({})", _DebugPath(barePath),
+               description);
+  bus_list.emplace_back(ValidReadyBus{
+      .hValid = std::move(hValid),
+      .hReady = std::move(hReady),
+      .description = description,
+      .onShakeCallback = onShake,
+  });
+}
+
+bool HandShakeDetector::ValidReadyBus::shakeHappened() {
+  return hValid.getUint32Value() == 1 && hReady.getUint32Value() == 1;
 }
 
 void HandShakeDetector::checkAndCountAll() {
-	for(auto &bus : bus_list){
-		if(bus.shakeHappened()){
-			bus.shake_count++;
-			logger->trace("Handshake happened on {} (total count {})",
-			              _DebugPath(bus.description), bus.shake_count);
-		}
+  for (auto &bus : bus_list) {
+    if (bus.shakeHappened()) {
+      bus.shake_count++;
+      logger->trace("Handshake happened on {} (total count {})",
+                    _DebugPath(bus.description), bus.shake_count);
+      if (bus.onShakeCallback) {
+        bus.onShakeCallback();
+      }
+    }
+  }
+}
+
+const char *InstTypeCounter::name_of_type(InstType type) {
+  static const char *type_names[] = {
+      "branch", "arithmetic", "load",  "store",  "jalr",
+      "jal",    "lui",        "auipc", "system",
+  };
+  if (type < sizeof(type_names) / sizeof(type_names[0])) {
+    return type_names[type];
+  } else {
+    return "unknown";
+  }
+}
+const char *InstTypeCounter::name_of_fmt(InstFmt fmt) {
+  static const char *fmt_names[] = {
+      "I_TYPE", "R_TYPE", "S_TYPE", "U_TYPE", "J_TYPE", "B_TYPE",
+  };
+  if (fmt < sizeof(fmt_names) / sizeof(fmt_names[0])) {
+    return fmt_names[fmt];
+  } else {
+    return "unknown";
+  }
+}
+void InstTypeCounter::init() {
+  logger = spdlog::stdout_color_mt("InstTypeCounter");
+  set_logger_pattern_with_simtime(logger);
+  logger->set_level(spdlog::level::info);
+  hInstType = SignalHandle("idu.iinfo_dec.io_out_typ");
+  hInstFmt = SignalHandle("idu.iinfo_dec.io_out_fmt");
+}
+void InstTypeCounter::newInstFetched(uint64_t cyc) {
+
+	if(isValidType(lastInstType)){
+		auto cycles = cyc - lastInstFetchCyc;
+		tot_cycle_of_type[lastInstType] += cycles;
 	}
+	if(isValidFmt(lastInstFmt)){
+		auto cycles = cyc - lastInstFetchCyc;
+		tot_cycle_of_fmt[lastInstFmt] += cycles;
+	}
+
+  uint32_t inst_type = hInstType.getUint32Value();
+  uint32_t inst_fmt = hInstFmt.getUint32Value();
+
+  type_count[inst_type]++;
+  fmt_count[inst_fmt]++;
+
+  logger->trace("new inst fetched: type {} fmt {}", inst_type, inst_fmt);
+
+	lastInstType = (InstType)inst_type;
+	lastInstFmt = (InstFmt)inst_fmt;
+	lastInstFetchCyc = cyc;
 }
--- npc/csrc/sig_event.hpp
+++ npc/csrc/sig_event.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "vpi_user.h"
+#include <cstddef>
+#include <cstdint>
+#include <numeric>
 #include <vector>
 #include <verilated.h>
 #include <verilated_vpi.h>
@@ -17,38 +20,124 @@ inline const std::string &cpu_vpi_path_prefix() {
   return prefix;
 }
 
-struct ValidReadyBus {
-  vpiHandle hValid;
-  vpiHandle hReady;
+struct SignalHandle {
+  vpiHandle handle;
+  ~SignalHandle() {
+    if (handle) {
+      vpi_release_handle(handle);
+    }
+  }
+  SignalHandle() : handle(nullptr) {}
+  SignalHandle(vpiHandle h) : handle(h) {}
 
-  std::string description;
+  SignalHandle(const SignalHandle &) = delete;
+  SignalHandle &operator=(const SignalHandle &) = delete;
+  SignalHandle(SignalHandle &&other) : handle(other.handle) {
+    other.handle = nullptr;
+  }
+  SignalHandle &operator=(SignalHandle &&other) {
+    if (this != &other) {
+      if (handle) {
+        vpi_release_handle(handle);
+      }
+      handle = other.handle;
+      other.handle = nullptr;
+    }
+    return *this;
+  }
 
-	size_t shake_count = 0;
+  SignalHandle(std::string barePath);
 
-  ValidReadyBus(vpiHandle hV, vpiHandle hR, std::string desc = "")
-			: hValid(hV), hReady(hR), description(desc) {}
-
-  bool shakeHappened() {
-    s_vpi_value valValid, valReady;
-    valValid.format = vpiIntVal;
-    valReady.format = vpiIntVal;
-
-    vpi_get_value(hValid, &valValid);
-    vpi_get_value(hReady, &valReady);
-
-    return (valValid.value.integer == 1) && (valReady.value.integer == 1);
+  uint32_t getUint32Value() {
+    s_vpi_value val;
+    val.format = vpiIntVal;
+    vpi_get_value(handle, &val);
+    return static_cast<uint32_t>(val.value.integer);
   }
 };
 
 class HandShakeDetector {
 public:
-	std::shared_ptr<spdlog::logger> logger;
+  using callback_t = std::function<void()>;
+  struct ValidReadyBus {
+    SignalHandle hValid;
+    SignalHandle hReady;
+
+    std::string description;
+
+    size_t shake_count = 0;
+    callback_t onShakeCallback = nullptr;
+
+    bool shakeHappened();
+  };
+  std::shared_ptr<spdlog::logger> logger;
   std::vector<ValidReadyBus> bus_list;
 
   HandShakeDetector();
 
-	void init();
-  void add(std::string pathWithoutValidOrReady, std::string description = "");
+  void init();
+  void add(std::string pathWithoutValidOrReady, std::string description = "",
+           callback_t onShake = nullptr);
 
-	void checkAndCountAll();
+  void checkAndCountAll();
+};
+
+struct InstTypeCounter {
+  // in common_def.scala
+  //   val imm, reg, store, upper, jump, branch = Value
+  //   val branch, arithmetic, load, store, jalr, jal, lui, auipc, system =
+  enum InstFmt { I_TYPE, R_TYPE, S_TYPE, U_TYPE, J_TYPE, B_TYPE, FMT_NUM };
+  enum InstType {
+    branch,
+    arithmetic,
+    load,
+    store,
+    jalr,
+    jal,
+    lui,
+    auipc,
+    system,
+    TYPE_NUM
+  };
+  static inline bool isValidFmt(InstFmt fmt) { return fmt < FMT_NUM; }
+  static inline bool isValidType(InstType type) { return type < TYPE_NUM; }
+
+  static const char *name_of_fmt(InstFmt fmt);
+  static const char *name_of_type(InstType type);
+
+  size_t fmt_count[FMT_NUM] = {0};
+  size_t type_count[TYPE_NUM] = {0};
+
+  size_t tot_cycle_of_type[TYPE_NUM] = {0};
+  size_t tot_cycle_of_fmt[FMT_NUM] = {0};
+
+  // init with invalid value
+  InstFmt lastInstFmt = FMT_NUM;
+  InstType lastInstType = TYPE_NUM;
+
+  uint64_t lastInstFetchCyc = 0;
+
+  SignalHandle hInstType;
+  SignalHandle hInstFmt;
+
+  std::shared_ptr<spdlog::logger> logger;
+
+  void init();
+
+  void newInstFetched(uint64_t cycle);
+
+  size_t totalInstCountSumByFmt() {
+    return std::accumulate(fmt_count, fmt_count + FMT_NUM, 0ull);
+  }
+  size_t totalInstCountSumByType() {
+    return std::accumulate(type_count, type_count + TYPE_NUM, 0ull);
+  }
+	double averageCPIOfType(InstType type){
+		if(type_count[type]==0) return NAN;
+		return (double)tot_cycle_of_type[type]/(double)type_count[type];
+	}
+	double averageCPIOfFmt(InstFmt fmt){
+		if(fmt_count[fmt]==0) return NAN;
+		return (double)tot_cycle_of_fmt[fmt]/(double)fmt_count[fmt];
+	}
 };

ab3708f4f021bf8534a0e8b3e2e81f81b5086bd8
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-02-01 12:43:24 +0800

    fix pc step 1 leads no counts opt use(0.8G->0.5G)

--- cachesim/public/src/itrace_pack.cc
+++ cachesim/public/src/itrace_pack.cc
@@ -6,19 +6,26 @@
 
 #define EXTERN_C extern "C"
 
+#define ONE_PC_STEP 4
+
 struct pc_record {
-  uint32_t pc;
+  uint32_t pc = 0;
   // count of consequtive occurrences
   // 0 means [pc]
   // 1 means [pc, pc + 1]
   // etc.
-  uint8_t count;
+  uint8_t count = 0;
   // TODO:
   // if count reaches 255, split into multiple records
 };
 
+struct PackHeader {
+  size_t size = 0;
+  size_t nRecords = 0;
+};
+
 struct itrace_pack_imp {
-  size_t size;
+  PackHeader header;
   FILE *fp;
   pc_record current;
 };
@@ -36,39 +43,34 @@ struct itrace_pack_imp {
 
 EXTERN_C itrace_pack_t itrace_pack_create(const char *filename) {
   itrace_pack_t pack = new itrace_pack_imp;
-  pack->current.pc = 0;
-  pack->current.count = 0;
-  pack->size = 0;
   pack->fp = fopen(filename, "wb");
   assert(pack->fp != nullptr);
   return pack;
 }
 static void load_one_record(itrace_pack_t pack) {
-	ASSERT_READ_ONE(&pack->current.pc);
-	ASSERT_READ_ONE(&pack->current.count);
+  ASSERT_READ_ONE(&pack->current.pc);
+  ASSERT_READ_ONE(&pack->current.count);
 }
 static void save_one_record(itrace_pack_t pack) {
-	ASSERT_WRITE_ONE(&pack->current.pc);
-	ASSERT_WRITE_ONE(&pack->current.count);
+  ASSERT_WRITE_ONE(&pack->current.pc);
+  ASSERT_WRITE_ONE(&pack->current.count);
+	pack->header.nRecords++;
 }
 static void init_from_file(itrace_pack_t pack) {
-  // jump to the end - size_t
-  fseek(pack->fp, -sizeof(size_t), SEEK_END);
-  // read size
-  auto n = fread(&pack->size, sizeof(size_t), 1, pack->fp);
-  assert(n == 1);
+  // jump to the end - header size
+  fseek(pack->fp, -sizeof(PackHeader), SEEK_END);
+  // read "HEADER"
+  ASSERT_READ_ONE(&pack->header);
+
   // jump to the beginning
   fseek(pack->fp, 0, SEEK_SET);
   // load the first record
-  if (pack->size > 0) {
+  if (pack->header.nRecords > 0) {
     load_one_record(pack);
   }
 }
 EXTERN_C itrace_pack_t itrace_pack_open(const char *filename) {
   itrace_pack_t pack = new itrace_pack_imp;
-  pack->current.pc = 0;
-  pack->current.count = 0;
-  pack->size = 0;
   pack->fp = fopen(filename, "rb");
   assert(pack->fp != nullptr);
   init_from_file(pack);
@@ -77,41 +79,45 @@ EXTERN_C itrace_pack_t itrace_pack_open(const char *filename) {
 EXTERN_C void itrace_pack_close(itrace_pack_t pack) {
   if (pack->current.pc != 0)
     save_one_record(pack);
-  fwrite(&pack->size, sizeof(size_t), 1, pack->fp);
+  // write header
+	printf("itrace_pack: saved %zu records, total pc record %zu\n", pack->header.nRecords, pack->header.size);
+  ASSERT_WRITE_ONE(&pack->header);
   fclose(pack->fp);
   delete pack;
 }
-EXTERN_C size_t itrace_pack_size(itrace_pack_t pack) { return pack->size; }
+EXTERN_C size_t itrace_pack_size(itrace_pack_t pack) {
+  return pack->header.size;
+}
+EXTERN_C size_t itrace_pack_nrecords(itrace_pack_t pack) {
+  return pack->header.nRecords;
+}
 EXTERN_C void itrace_pack_add(itrace_pack_t pack, uint32_t pc) {
-  if (pack->current.pc + pack->current.count + 1 == pc) {
+  if (pack->current.pc + pack->current.count + ONE_PC_STEP == pc) {
     if (pack->current.count == 0xff) {
       assert(false && "itrace_pack_add: pc_record count overflow unhandled");
     }
     pack->current.count += 1;
   } else {
+		// printf("consecutive pc broken: prev=%08x count=%u new=%08x\n", pack->current.pc, pack->current.count, pc);
     if (pack->current.pc != 0)
       save_one_record(pack);
     pack->current.pc = pc;
     pack->current.count = 0;
   }
-  pack->size += 1;
+  pack->header.size += 1;
 }
 EXTERN_C uint32_t itrace_pack_pickone(itrace_pack_t pack) {
-  if (pack->size == 0) {
+  if (pack->header.size == 0) {
     return 0;
   }
-  uint32_t pc = pack->current.pc + pack->current.count;
+  uint32_t pc = pack->current.pc + pack->current.count*ONE_PC_STEP;
   if (pack->current.count == 0) {
-    // if(pack->current.count >= 0xff){
-    // 	fprintf(stderr, "Warning: pc_record count count huge at pc=0x%08x\n",
-    // pack->current.pc);
-    // }
-    if (pack->size > 1) {
+    if (pack->header.size > 1) {
       load_one_record(pack);
     }
   } else {
     pack->current.count -= 1;
   }
-  pack->size -= 1;
+  pack->header.size -= 1;
   return pc;
 }

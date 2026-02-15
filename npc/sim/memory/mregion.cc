4ef0cb49bbf661d8eeae62f8815d36e7ebd97cc4
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-02-15 19:52:48 +0800

    fix make perf unset MAKE_PERF fix mem logger sinks empty

--- npc/sim/memory/mregion.cc
+++ npc/sim/memory/mregion.cc
@@ -1,7 +1,18 @@
 #include "mregion.hpp"
 #include "../sim.hpp"
 #include <cstdint>
+#include <memory>
+#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
+
+auto _GetLoggerForRegion(std::string_view region_name) {
+  auto logger = spdlog::get(region_name.data());
+  if (!logger) {
+    spdlog::warn("logger for mem region '{}' not found", region_name);
+    return spdlog::default_logger();
+  }
+  return logger;
+};
 
 void init_mem_logger() {
   auto lvl = spdlog::level::info;
@@ -9,8 +20,8 @@ void init_mem_logger() {
 #define _REG_MEM_REGION_LOGGER(name)                                           \
   do {                                                                         \
     spdlog::debug("Registering logger for mem region '{}'", #name);            \
-    auto logger =                                                              \
-        std::make_shared<spdlog::logger>(#name, spdlog::sinks_init_list{});    \
+    auto logger = std::make_shared<spdlog::logger>(                            \
+        #name, std::make_shared<spdlog::sinks::stdout_color_sink_mt>());       \
     logger->set_level(lvl);                                                    \
     set_logger_pattern_with_simtime(logger);                                   \
     spdlog::register_logger(logger);                                           \
@@ -25,11 +36,9 @@ void init_mem_logger() {
 void mem_region_traits::assert_in_range(uint32_t addr) const {
   bool in_range = contains(addr);
   if (!in_range) {
-    auto logger = spdlog::get(name.data());
-    if (logger) {
-      logger->error("addr {:08x} out of bound for region {} [{:08x}, {:08x})",
-                    addr, name, _Base, _End);
-    }
+    _GetLoggerForRegion(name)->error(
+        "addr {:08x} out of bound for region {} [{:08x}, {:08x})", addr, name,
+        _Base, _End);
   }
   assert(in_range);
 }
@@ -37,12 +46,10 @@ void mem_region_traits::assert_in_range(uint32_t addr) const {
 void direct_mapped_mem::assert_in_actual_data_range(uint32_t addr) const {
   size_t offset = addr - _Base;
   if (offset >= _ActualSizeInBytes) {
-    auto logger = spdlog::get(this->name.data());
-    if (logger) {
-      logger->error("addr {:08x} out of actual data bound for region {} "
-                    "[0, {:08x})",
-                    addr, this->name, _ActualSizeInBytes);
-    }
+    _GetLoggerForRegion(name)->error(
+        "addr {:08x} out of actual data bound for region {} "
+        "[0, {:08x})",
+        addr, this->name, _ActualSizeInBytes);
   }
   assert(offset < _ActualSizeInBytes);
 }
@@ -54,10 +61,6 @@ sdram_mem::u32_data_ptr sdram_mem::get_data_at(uint32_t addr) {
   uint16_t col = (in_sdram_addr >> 1) & 0x1ff;
   uint8_t bank = raw_bank % 4;
   uint8_t block = (raw_bank & 0x4) ? 2 : 0;
-  assert(bank < N_BANKS);
-  assert(row < N_ROWS);
-  assert(col < N_COLS);
-  assert(block < N_BLOCKS);
   return {.lowpart = &data_at(bank, row, col, block),
           .highpart = &data_at(bank, row, col, block + 1)};
 }
@@ -66,4 +69,23 @@ uint8_t *sdram_mem::get_data_ptr_at(uint32_t addr) {
   spdlog::error(
       "unimpled!!! sdram region does not support direct data pointer access");
   return nullptr;
+}
+
+#define ASSERT_BELOW(val, limit)                                               \
+  do {                                                                         \
+    if ((val) >= (limit)) {                                                    \
+      _GetLoggerForRegion(name)->error(                                        \
+          "value " #val " should be below " #limit "={}, but got {}", val,     \
+          limit, (val));                                                       \
+    }                                                                          \
+    assert((val) < (limit));                                                   \
+  } while (0)
+uint16_t &sdram_mem::data_at(size_t bank, size_t row, size_t col,
+                             size_t block) {
+  ASSERT_BELOW(bank, N_BANKS);
+  ASSERT_BELOW(row, N_ROWS);
+  ASSERT_BELOW(col, N_COLS);
+  ASSERT_BELOW(block, N_BLOCKS);
+  return (*mem_container)[bank * (N_ROWS * N_COLS * N_BLOCKS) +
+                          row * (N_COLS * N_BLOCKS) + col * N_BLOCKS + block];
 }

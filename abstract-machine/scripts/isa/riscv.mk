850077a561a04f6fd8eca7edf561d7b2d9d76d38
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-02-28 10:14:55 +0800

    move llvm install into npc init

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -4,31 +4,6 @@ CFLAGS        += $(COMMON_CFLAGS) -static
 ASFLAGS       += $(COMMON_CFLAGS) -O0 -g
 LDFLAGS       += -melf64lriscv
 
-CLANG_VERSION_MAJOR := $(shell clang -dumpversion | cut -f1 -d.)
-CLANG_VERSION_OLDER_THAN_15 := $(shell [ $(CLANG_VERSION_MAJOR) -lt 15 ] && echo 1 || echo 0)
-
-LLVM21_INSTALLED = ./llvm21_installed.done
-$(LLVM21_INSTALLED):
-	$(info # Downloading LLVM script)
-	$(shell wget 'https://apt.llvm.org/llvm.sh' && chmod +x llvm.sh)
-	$(info # Installing LLVM 21)
-	$(shell sudo ./llvm.sh 21)
-	$(info # Setting LLVM 21 as default clang)
-	$(shell sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-21 100 --slave /usr/bin/clang++ clang++ /usr/bin/clang++-21)
-	touch $@
-
-foo.mk: $(LLVM21_INSTALLED)
-
-include foo.mk
-
-ifeq ($(CLANG_VERSION_OLDER_THAN_15), 1)
-# older clang versions contains csr and fence.i extensions in the rv32i/e base
-# RISCV_MARCH_EXT_CSRS_AND_FENCE_I :=
-# COMMON_CFLAGS += -Wno-unused-command-line-argument
-$(info fuck clang $(CLANG_VERSION_MAJOR))
-$(info Try clang 21!)
-else
-endif
 RISCV_MARCH_EXT_CSRS_AND_FENCE_I := _zicsr_zifencei
 	
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile

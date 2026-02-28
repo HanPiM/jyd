60fe603946da62162fd2b68247e91be4cd114d80
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-02-28 09:27:55 +0800

    support older version clang no support zicsr/fencei

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -4,5 +4,15 @@ CFLAGS        += $(COMMON_CFLAGS) -static
 ASFLAGS       += $(COMMON_CFLAGS) -O0 -g
 LDFLAGS       += -melf64lriscv
 
+CLANG_VERSION_MAJOR := $(shell clang -dumpversion | cut -f1 -d.)
+CLANG_VERSION_OLDER_THAN_15 := $(shell [ $(CLANG_VERSION_MAJOR) -lt 15 ] && echo 1 || echo 0)
+
+ifeq ($(CLANG_VERSION_OLDER_THAN_15), 1)
+# older clang versions contains csr and fence.i extensions in the rv32i/e base
+RISCV_MARCH_EXT_CSRS_AND_FENCE_I :=
+else
+RISCV_MARCH_EXT_CSRS_AND_FENCE_I := _zicsr_zifencei
+endif
+	
 # overwrite ARCH_H defined in $(AM_HOME)/Makefile
 ARCH_H := arch/riscv.h

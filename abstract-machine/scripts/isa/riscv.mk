81f0a847a11178d34f128ccc6e0e6650763a036e
-Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-02-28 09:41:01 +0800

    add -Wno-unused-command-line-argument

--- abstract-machine/scripts/isa/riscv.mk
+++ abstract-machine/scripts/isa/riscv.mk
@@ -10,6 +10,7 @@ CLANG_VERSION_OLDER_THAN_15 := $(shell [ $(CLANG_VERSION_MAJOR) -lt 15 ] && echo
 ifeq ($(CLANG_VERSION_OLDER_THAN_15), 1)
 # older clang versions contains csr and fence.i extensions in the rv32i/e base
 RISCV_MARCH_EXT_CSRS_AND_FENCE_I :=
+COMMON_CFLAGS += -Wno-unused-command-line-argument
 else
 RISCV_MARCH_EXT_CSRS_AND_FENCE_I := _zicsr_zifencei
 endif

855761384a79e1aa07505068af841ada241297c5
Wang Yi Jie <miwyj@mail.scut.edu.cn>
2026-05-25 00:41:17 +0800

    Revert "add latency for div IP input"
    This reverts commit 6f34263bc768199cdef789dd7a692d2835a7591a.

--- npc/design/src/alu.scala
+++ npc/design/src/alu.scala
@@ -204,19 +204,17 @@ class Divider extends Module {
   })
 
   object State extends ChiselEnum {
-    val idle, launch, busy, done = Value
+    val idle, busy, done = Value
   }
   val state = RegInit(State.idle)
 
-  val func3tReg        = Reg(UInt(3.W))
-  val quotientNegReg   = Reg(Bool())
-  val remainderNegReg  = Reg(Bool())
-  val specialReg       = Reg(Bool())
+  val func3tReg      = Reg(UInt(3.W))
+  val quotientNegReg = Reg(Bool())
+  val remainderNegReg = Reg(Bool())
+  val specialReg     = Reg(Bool())
   val specialResultReg = Reg(Types.UWord)
-  val dividendAbsReg   = Reg(Types.UWord)
-  val divisorAbsReg    = Reg(Types.UWord)
-  val resultReg        = Reg(Types.UWord)
-  val divider          = Module(new div_gen_uradix2)
+  val resultReg      = Reg(Types.UWord)
+  val divider        = Module(new div_gen_uradix2)
 
   val inputFunc3t = io.in.bits.func3t
   val inputIsRem = inputFunc3t(1)
@@ -232,13 +230,13 @@ class Divider extends Module {
   val inputOverflowResult = Mux(inputIsRem, 0.U, "h80000000".U)
   val inputSpecialResult = Mux(inputDivideByZero, inputDivideByZeroResult, inputOverflowResult)
 
-  val ipFire = state === State.launch && !specialReg
+  val ipFire = io.in.fire && !inputSpecial
 
   divider.io.aclk                   := clock
   divider.io.s_axis_divisor_tvalid  := ipFire
-  divider.io.s_axis_divisor_tdata   := divisorAbsReg
+  divider.io.s_axis_divisor_tdata   := Mux(inputDivisorAbs === 0.U, 1.U, inputDivisorAbs)
   divider.io.s_axis_dividend_tvalid := ipFire
-  divider.io.s_axis_dividend_tdata  := dividendAbsReg
+  divider.io.s_axis_dividend_tdata  := inputDividendAbs
 
   val ipResult = divider.io.m_axis_dout_tdata
   val ipQuotient = ipResult(63, 32)
@@ -249,41 +247,26 @@ class Divider extends Module {
   val resultValid = divider.io.m_axis_dout_tvalid
 
   io.in.ready  := state === State.idle
-  io.out.valid := (state === State.done) || ((state === State.launch) && specialReg) ||
-    ((state === State.busy) && resultValid)
-  io.out.bits := Mux(state === State.done, resultReg, Mux(specialReg, specialResultReg, result))
+  io.out.valid := (state === State.done) || ((state === State.busy) && (specialReg || resultValid))
+  io.out.bits  := Mux(state === State.done, resultReg, Mux(specialReg, specialResultReg, result))
 
   switch(state) {
     is(State.idle) {
       when(io.in.fire) {
-        func3tReg        := io.in.bits.func3t
-        quotientNegReg   := inputDividendNeg ^ inputDivisorNeg
-        remainderNegReg  := inputDividendNeg
-        specialReg       := inputSpecial
+        func3tReg       := io.in.bits.func3t
+        quotientNegReg  := inputDividendNeg ^ inputDivisorNeg
+        remainderNegReg := inputDividendNeg
+        specialReg      := inputSpecial
         specialResultReg := inputSpecialResult
-        dividendAbsReg   := inputDividendAbs
-        divisorAbsReg    := Mux(inputDivisorAbs === 0.U, 1.U, inputDivisorAbs)
-        state            := State.launch
-      }
-    }
-    is(State.launch) {
-      when(specialReg) {
-        when(io.out.ready) {
-          state := State.idle
-        }.otherwise {
-          resultReg := specialResultReg
-          state     := State.done
-        }
-      }.otherwise {
-        state := State.busy
+        state           := State.busy
       }
     }
     is(State.busy) {
-      when(resultValid) {
+      when(specialReg || resultValid) {
         when(io.out.ready) {
           state := State.idle
         }.otherwise {
-          resultReg := result
+          resultReg := Mux(specialReg, specialResultReg, result)
           state     := State.done
         }
       }

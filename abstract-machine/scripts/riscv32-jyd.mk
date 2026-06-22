0db2a9800efe8e78256c81e8bce0788112f9aa2e
hanpi <hanpi@localhost.localdomain>
2026-06-22 22:05:38 +0800

    add riscv z series flags

--- abstract-machine/scripts/riscv32-jyd.mk
+++ abstract-machine/scripts/riscv32-jyd.mk
@@ -1,6 +1,8 @@
 include $(JYD_AM_HOME)/scripts/isa/riscv.mk
 include $(JYD_AM_HOME)/scripts/platform/jyd.mk
-COMMON_CFLAGS += -march=rv32im_zicsr_zba -mabi=ilp32  # overwrite
+
+RISCV_ZEXTS = _zba_zbb_zbc_zbs_zbkb_zbkx
+COMMON_CFLAGS += -march=rv32im_zicsr_$(RISCV_ZEXTS) -mabi=ilp32  # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
 AM_SRCS += riscv/npc/libgcc/div.S \
--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -1,7 +1,9 @@
 include $(JYD_AM_HOME)/scripts/isa/riscv.mk
 include $(JYD_AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
-COMMON_CFLAGS += -march=rv32im$(RISCV_MARCH_EXT_CSRS_AND_FENCE_I)_zba -mabi=ilp32   # overwrite
+
+RISCV_ZEXTS = _zba_zbb_zbc_zbs_zbkb_zbkx
+COMMON_CFLAGS += -march=rv32im$(RISCV_MARCH_EXT_CSRS_AND_FENCE_I)$(RISCV_ZEXTS) -mabi=ilp32   # overwrite
 LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/nemu/start.S \

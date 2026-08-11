a91597abc635a0feaf1e616c7fcab0a2c036a5e7
hanpi <hanpi@localhost.localdomain>
2026-08-12 02:56:26 +0800

    npc: restrict fast tokens to integer opcodes

--- npc/design/src/idu.scala
+++ npc/design/src/idu.scala
@@ -387,12 +387,12 @@ class IDU(
   res.numericDfaValid := isNumericDfaCustom
   res.aluIsSub  := !isFmtI && inst(30)
   val isPack = !isFmtI && arithmeticFunc3 === "b100".U && arithmeticFunc7 === "b0000100".U
-  val isRv32iImmediate = isFmtI && (
+  val isRv32iImmediate = inst(6, 0) === "b0010011".U && isFmtI && (
     (arithmeticFunc3 =/= "b001".U && arithmeticFunc3 =/= "b101".U) ||
       (arithmeticFunc3 === "b001".U && arithmeticFunc7 === 0.U) ||
       (arithmeticFunc3 === "b101".U && (arithmeticFunc7 === 0.U || arithmeticFunc7 === "b0100000".U))
   )
-  val isRv32iRegister = !isFmtI && (
+  val isRv32iRegister = inst(6, 0) === "b0110011".U && !isFmtI && (
     arithmeticFunc7 === 0.U ||
       (arithmeticFunc7 === "b0100000".U && (arithmeticFunc3 === 0.U || arithmeticFunc3 === "b101".U))
   )
